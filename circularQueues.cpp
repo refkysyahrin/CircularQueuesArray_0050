da8bfec659492012e38f0dd830e45956162741b1
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-05-30 18:24:41 +0700

    Cek apakah antrian kosong

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -62,5 +62,12 @@ public:
 
     void display() {
         int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        // Cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
     }
 }
