d7fdaabdd8ae8ec40b9a533c445a2bc7ad3876ae
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-05-30 18:35:46 +0700

    Iterasi dari awal array hingga REAR

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -85,7 +85,17 @@ public:
             while (FRONT_position <= max - 1) {
                 cout << queue_array[FRONT_position] << "   ";
                 FRONT_position++;
+               
             }
+            
+            FRONT_position = 0;
+
+            // Iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+            cout << endl;
         }
     }
 }
