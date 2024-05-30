4d0212a32acc55a1c9df39511ebc1c662117fe02
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-05-30 17:39:08 +0700

    overflow

--- circularQueves.cpp
+++ circularQueves.cpp
@@ -18,6 +18,8 @@ public:
         cout << endl;
 
         // Cek apakah antrian penuh
-        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) 
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+        }
     }
 }
