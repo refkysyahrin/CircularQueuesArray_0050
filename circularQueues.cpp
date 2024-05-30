46bec8e76a6446be14f958f48fb22ff479f84aa3
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-05-30 18:14:37 +0700

    Cek jika antrian hanya memiiki satu antrian

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -42,6 +42,14 @@ public:
         // Cek apakah antrian kosong
         if (FRONT == -1) {
             cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        // Cek jika antrian hanya memiiki satu antrian
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
         }
     }
 }
